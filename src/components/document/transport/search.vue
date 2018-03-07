f4297f647d5e384298c56f21758339ab882396ac
zaoyu63 <zaoyu63@163.com>
2018-03-07 11:31:43 +0800

    优化地图选择

--- src/components/document/transport/search.vue
+++ src/components/document/transport/search.vue
@@ -4,17 +4,17 @@
       <el-col :span="2">
         <h2 class="header f-dib">运单查询</h2>
       </el-col>
-      <el-col :span="18" class="text-right">
-          <span class="switching-icon" @click="showSearch = !showSearch">
-            <i class="el-icon-arrow-up"></i>
-            <span v-show="showSearch">收起筛选</span>
-            <span v-show="!showSearch">展开筛选</span>
-          </span>
-      </el-col>
-      <el-col :span="4" class="text-right">
+      <!--<el-col :span="18" class="text-right">-->
+          <!--<span class="switching-icon" @click="showSearch = !showSearch">-->
+            <!--<i class="el-icon-arrow-up"></i>-->
+            <!--<span v-show="showSearch">收起筛选</span>-->
+            <!--<span v-show="!showSearch">展开筛选</span>-->
+          <!--</span>-->
+      <!--</el-col>-->
+      <el-col :span="22" class="text-right">
         <el-button-group>
           <el-button type="primary" plain size="small" @click="search">查询结果</el-button>
-          <el-button type="primary" plain size="small" @click="showSearch = true">高级检索</el-button>
+          <el-button type="primary" plain size="small" @click="showSearch = !showSearch">高级检索</el-button>
         </el-button-group>
       </el-col>
     </div>
