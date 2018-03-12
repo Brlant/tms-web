198d3707bc6d773899e1d2fb1bd3bc1a2c6f8776
zhouhao <798804880@qq.com>
2018-03-12 11:26:34 +0800

    修改页面

--- src/components/document/order/form/show-form.vue
+++ src/components/document/order/form/show-form.vue
@@ -184,7 +184,7 @@
           </div>
           <div class="content">
             <el-table :data="form.goodsList" border style="width: 100%">
-              <el-table-column prop="goodsName" label="货品名称">
+              <el-table-column prop="goodsName" label="货品名称" width="200">
               </el-table-column>
               <el-table-column prop="weight" label="货品重量">
                 <template slot-scope="scope">
