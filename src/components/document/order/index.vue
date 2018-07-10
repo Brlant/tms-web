0e897835c963a14b13132e89d176758c30d16980
zaoyu63 <zaoyu63@163.com>
2018-07-10 13:31:28 +0800

    合计，切换状态，加载时隐藏

--- src/components/document/order/index.vue
+++ src/components/document/order/index.vue
@@ -161,7 +161,7 @@
           <div class="order-list-item-bg"></div>
         </div>
       </div>
-      <el-row class="order-list-header" v-show="dataList.length">
+      <el-row class="order-list-header" v-show="dataList.length && !loadingData">
         <el-col :span="15" align="left">合计</el-col>
         <el-col :span="1">{{totalCount.whole}}</el-col>
         <el-col :span="1">{{totalCount.buck}}</el-col>
