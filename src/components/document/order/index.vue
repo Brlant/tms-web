7a084aad6dc63f9565720dadcc78900400107d93
zhouhao <798804880@qq.com>
2018-08-10 15:00:46 +0800

    订单待派车可编辑

--- src/components/document/order/index.vue
+++ src/components/document/order/index.vue
@@ -125,14 +125,14 @@
               <div>
                 <div>
                   <perm label="tms-order-edit" class="btn-line-block">
-                    <span @click.stop="edit(item)" v-if="activeStatus===0||activeStatus==='0'">
+                    <span @click.stop="edit(item)" v-if="activeStatus==='0'||activeStatus==='1'">
                       <a @click.pervent="" class="btn-circle btn-opera">
                         <i class="el-icon-t-edit"></i>
                       </a>编辑
                     </span>
                   </perm>
                   <perm label="tms-order-delete" class="btn-line-block">
-                    <span @click.stop="deleteOrder(item)" v-if="activeStatus===0||activeStatus==='0'">
+                    <span @click.stop="deleteOrder(item)" v-if="activeStatus==='0'">
                       <a @click.pervent="" class="btn-circle btn-opera">
                         <i class="el-icon-t-delete"></i>
                       </a>删除
@@ -141,14 +141,14 @@
                 </div>
                 <div style="padding-top: 2px">
                   <perm label="tms-order-cancel" class="opera-btn btn-line-block">
-                    <span @click.stop="cancelOrder(item)" v-if="activeStatus===0||activeStatus==='0'">
+                    <span @click.stop="cancelOrder(item)" v-if="activeStatus==='0'">
                       <a @click.pervent="" class="btn-circle btn-opera">
                         <i class="el-icon-t-forbidden"></i>
                       </a>取消
                     </span>
                   </perm>
                   <perm label="tms-order-dismantling" class="opera-btn btn-line-block">
-                    <span @click.stop="splitOrder(item)" v-if="activeStatus===0||activeStatus==='0'">
+                    <span @click.stop="splitOrder(item)" v-if="activeStatus==='0'">
                       <a @click.pervent="" class="btn-circle btn-opera">
                         <i class="el-icon-t-basic"></i>
                       </a>拆分订单
