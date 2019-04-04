3c6e2b10af45accbe246e0934cbd1b1df3493ea7
kcs <dr_cskuang@163.com>
2019-04-04 16:17:48 +0800

    过滤掉错误日志

--- src/main.js
+++ src/main.js
@@ -30,7 +30,19 @@ if (process.env.NODE_ENV === 'production') {
   // 日志工具
   Raven
     .config('https://62f56ceea555483fab3d6238ff4a80d0@f-log.cdcerp.net/3', {
-      serverName: 'tms'
+      serverName: 'tms',
+      // debug: true,
+      shouldSendCallback: (date) => { // 过滤错误日志
+        // 开发调试过程不发送错误日志
+        // if (process.env.NODE_ENV !== 'production') {
+        //   return false;
+        // }
+        let filterArray = ['Request failed with status code 401', 'Request failed with status code 502'];
+        if (date.exception.values && filterArray.indexOf(date.exception.values[0].value) > -1) {
+          return false;
+        }
+        return date;
+      }
     })
     .addPlugin(RavenVue, Vue)
     .install();
