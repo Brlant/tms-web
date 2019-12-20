8b66d03c3ed5ddaa19ace139f2340fcd240aada2
zaoyu63 <zaoyu63@163.com>
2019-12-20 14:04:25 +0800

    修改运单管理详情，保温箱列表，新增，温度计列表查询页面卡死问题

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
     https: false,
     hotOnly: false, disableHostCheck: true, proxy: {
       '/api': {
-        target: 'https://tms.cnvss.net/',
+        target: 'https://test-tms.cdcerp.cn/',
         changOrigin: true
       }
     }
